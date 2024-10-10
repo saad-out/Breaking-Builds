649890308d10f01de18969f17f7dacf381354241
saad-out <outsaad03@gmail.com>
2024-10-10 17:26:27 +0100

    handle exception logic in routes functions & get builds file from .env

--- app/backend/my_utils.py
+++ app/backend/my_utils.py
@@ -1,17 +1,21 @@
 from json import load, dump, JSONDecodeError
 from datetime import datetime
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BUILDS_FILE_NAME = os.environ.get("BUILDS_FILE_NAME", "builds.json")
 
 def write_build_to_json():
     """
     Write a build to builds.json
     """
-    FILE_NAME = "builds.json"
-    if not os.path.exists(FILE_NAME):
-        with open(FILE_NAME, "w") as f:
+    if not os.path.exists(BUILDS_FILE_NAME):
+        with open(BUILDS_FILE_NAME, "w") as f:
             dump({}, f)
 
-    with open(FILE_NAME, "r") as f:
+    with open(BUILDS_FILE_NAME, "r") as f:
         try:
             data = load(f)
         except JSONDecodeError:
@@ -21,5 +25,5 @@ def write_build_to_json():
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     data[len(data)] = current_time
 
-    with open(FILE_NAME, "w") as f:
+    with open(BUILDS_FILE_NAME, "w") as f:
         dump(data, f)
