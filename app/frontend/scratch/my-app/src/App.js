6a91ab5a63f6094da1a50faf5cd0372680982fd3
saad-out <outsaad03@gmail.com>
2024-11-05 18:15:07 +0100

    UI data fetched from dummy API

--- app/frontend/scratch/my-app/src/App.js
+++ app/frontend/scratch/my-app/src/App.js
@@ -1,28 +1,79 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import StageCards from './StageCards';
-import data from './data.json'; // Importing the dummy data
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+    triggerBuild,
+    getBuildQueueState,
+    getBuildStages,
+} from './api';
+
 
 function App() {
-  const [showStages, setShowStages] = useState(false);
-  const [loading, setLoading] = useState(false);
+    const [showStages, setShowStages] = useState(false);
+    const [stages, setStages] = useState([]); // TODO: load with initial empty data
+    const [buildNumber, setBuildNumber] = useState(-1);
+    const [triggerButtonState, setTriggerButtonState] = useState('Trigger');
+    
+    const handleButtonClick = async () => {
+        if (triggerButtonState === 'Trigger') {
 
-  const handleButtonClick = () => {
-    setLoading(true);
-    setShowStages(true);
-  };
+            setTriggerButtonState('Triggering...');
+            const data = await triggerBuild();
+            const queue_id = data['body']['info']['queue_id'];
 
-  return (
-    <div className="container">
-      <h1 className="title">Breaking Builds</h1>
-      <p className="description">Realtime Full CI/CD Pipeline with Jenkins</p>
-      <button className={loading ? "button-disabled" : "trigger-button" } onClick={handleButtonClick} disabled={loading}>
-        { loading ? 'Triggering...' : 'Trigger Build' }
-      </button>
-      {showStages && <StageCards stages={data} />}
-    </div>
-  );
+            const interval = setInterval(async () => {
+                console.log('Checking build queue state');
+                const data = await getBuildQueueState(queue_id);
+                if (data['body']['state'] === 'left')
+                {
+                    setBuildNumber(data['body']['build_number']);
+                    setTriggerButtonState('Triggered ✅');
+                    clearInterval(interval);
+                }
+                else if (data['body']['state'] === 'cancelled')
+                {
+                    setTriggerButtonState('Error ❌');
+                    clearInterval(interval);
+                }
+            }, 1000);
+        }
+    };
+
+    useEffect(() => {
+        if (buildNumber !== -1) {
+            const interval = setInterval(async () => {
+                console.log('Fetching build stages');
+                const data = await getBuildStages(buildNumber);
+                setStages(data['body']['stages']);
+                setShowStages(true);
+                let allDone = true;
+                for (let stage of data['body']['stages']) {
+                    if (stage['state'] !== 'done') {
+                        allDone = false;
+                        break;
+                    }
+                }
+                if (allDone) {
+                    clearInterval(interval);
+                }
+            }, 1000);
+
+            // Avoid memory leak
+            return () => clearInterval(interval);
+        }
+    }, [buildNumber]);
+
+    return (
+        <div className="container">
+        <h1 className="title">Breaking Builds</h1>
+        <p className="description">Realtime Full CI/CD Pipeline with Jenkins</p>
+        <button className={ (triggerButtonState !== 'Trigger') ? "button-disabled" : "trigger-button" } onClick={handleButtonClick} disabled={ (triggerButtonState !== 'Trigger') }>
+            { (triggerButtonState === 'Trigger') ? 'Trigger Build 👨‍🍳' : triggerButtonState }
+        </button>
+        {showStages && <StageCards buildNumber={buildNumber} stages={stages} />}
+        </div>
+    );
 }
 
 export default App;
--- app/frontend/scratch/my-app/src/StageCards.js
+++ app/frontend/scratch/my-app/src/StageCards.js
@@ -2,13 +2,39 @@
 import React, { useState } from 'react';
 import './App.css';
 import CustomModal from './Modal';
+import { getStageSteps } from './api';
 
-const StageCards = ({ stages }) => {
+const StageCards = ({ buildNumber, stages }) => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [currentSteps, setCurrentSteps] = useState([]);
+  const [steps, setSteps] = useState({});
+  const [currentSteps, setCurrentSteps] = useState([]); // TODO: load with initial empty data
 
-  const openModal = (steps) => {
-    setCurrentSteps(steps);
+  const openModal = (stage) => {
+      if (stage['state'] === 'done' && !(stage['id'] in steps)) {
+          const interval = setInterval(async () => {
+              console.log('Fetching stage steps for stage:', stage['id']);
+              const data = await getStageSteps(buildNumber, stage['id']);
+              let _steps = data['body']['steps'];
+              _steps["stageId"] = stage['id'];
+              _steps["done"] = true;
+              setCurrentSteps(_steps);
+              console.log('steps:', _steps);
+              let allDone = true;
+              for (let step of _steps) {
+                  if (step['state'] !== 'done') {
+                      allDone = false;
+                      break;
+                  }
+              }
+              if (allDone) {
+                  steps[stage['id']] = _steps;
+                  setSteps(steps);
+                  clearInterval(interval);
+              }
+          }, 1000);
+      } else if (stage['state'] !== 'done') {
+          setCurrentSteps({"done": false});
+      }
     setModalIsOpen(true);
   };
 
@@ -18,17 +44,27 @@ const StageCards = ({ stages }) => {
 
   return (
     <div className="stage-cards-container">
-      {stages.map((stage, index) => {
-        const stageTitle = Object.keys(stage)[0];
-        const steps = stage[stageTitle];
+      {
+           stages.map((stage, index) => {
+            const stageTitle = stage['name'];
+            const stageState = stage['state'];
+            const _stage = {
+                "id": stage['id'],
+                "title": stageTitle,
+                "state": stageState,
+                "steps": [],
+            };
 
-        return (
-          <div key={index} className="stage-card" onClick={() => openModal(steps)}>
-            <h2 className="stage-title">{stageTitle}</h2>
-          </div>
-        );
-      })}
+            return (
+            <div key={index} className="stage-card" onClick={() => openModal(_stage)}>
+                <h2 className="stage-title">{stageTitle}</h2>
+                <h5>{stageState}</h5>
+            </div>
+            );
+        })
+      }
       <CustomModal
+        buildNumber={buildNumber}
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
         steps={currentSteps}
